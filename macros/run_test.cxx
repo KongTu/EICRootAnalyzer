62ae7a5e05439e9ad64676933f5b89f48aebf71c
Kong Tu <kongkong@rice.edu>
2018-07-24 14:36:04 -0400

    initial commit

---
+++ macros/run_test.cxx
@@ -0,0 +1,90 @@
+
+// read.cxx
+//
+// Created by TB on 6/13/11.
+// Copyright 2011 BNL. All rights reserved.
+//
+// Example of how to read a file produced by BuildTree for a simple analysis.
+// To run, in ROOT do:
+// root [0] .L /path/to/read.cxx
+// root [1] read("myInputFile.root", 10000 )
+
+void run_test(TString inFileNames, int nEvents ) {
+   
+   // If the analysis solely uses TTree::Draw statements, you don't need to load
+   // the shared library. You will receive warnings such as
+   // Warning in <TClass::TClass>: no dictionary for class Particle is available
+   // but these can be ignored. However if you wish to work with the event
+   // objects themselves, the shared library must be loaded:
+   //gSystem->Load("/afs/rhic.bnl.gov/eic/MACROS/BuildTree/BuildTree.so" ); // To use the master version
+   gSystem->Load("../lib/BuildTree.so" ); // To use your own compiled version
+   
+   // The TTrees are named EICTree.
+   // Create a TChain for trees with this name.
+   TChain tree("EICTree" );
+   
+   // Add the file(s) we want to analyse to the chain.
+   // We could add multiple files if we wanted.
+   tree.Add(inFileNames ); // Wild cards are allowed e.g. tree.Add("*.root" );
+// tree.Add(/path/to/otherFileNames ); // etc... 
+   
+   // Create an object to store the current event from the tree.
+   // This is how we access the values in the tree.
+   // If you want to use generator-specific values, then
+   // the event type should match the type in the TTree. Valid types are
+   // EventPythia, EventPepsi, EventRapgap, EventDjangoh, EventMilou.
+   // If you only need shared quantities like x, Q2 and the particle list
+   // you can use EventBase and the macro will be general for any Monte Carlo.
+   EventPythia* event(NULL);// = new EventPythia;
+   //EventBase* event(NULL);
+   
+   // Now associate the contents of the branch with the buffer.
+   // The events are stored in a branch named event:
+   tree.SetBranchAddress("event", &event ); // Note &event, not event.
+      
+   // Now we can do some analysis...
+   
+   // We record the largest particle pT we find here:
+   //double highestPt(-1. );
+   
+   // Histograms for our analysis.
+   TH1D ptHist("ptHist",
+               "pT of charged pions",
+               500, 0.0, 10 );
+   TH1D statusHist("statusHist",
+               "status distribution  ",
+               50, 0, 50 );
+   // Loop over events:
+   for(int i(0); i < nEvents; ++i ) {
+      
+      // Read the next entry from the tree.
+      tree.GetEntry(i);
+      
+      // The event contains a vector (array) of particles.
+      int nParticles = event->GetNTracks();
+      //cout << "nParticles: " << nParticles << endl;
+      //cout << "t_hat: " << event->GetHardT() << endl;
+      double total_pT = 0.0;
+
+      // We now know the number of particles in the event, so loop over
+      // the particles:
+      for(int j(0); j < nParticles; ++j ) {
+         const erhic::ParticleMC* particle = event->GetTrack(j);
+	 
+	 // Let's just select charged pions for this example:
+         int pdg = particle->GetPdgCode();
+         int status = particle->GetStatus();
+         //(abs(pdg) != 211 ) continue;
+         
+	 //total_pT += particle->GetPt();   
+	 ptHist.Fill(particle->GetPt());
+         statusHist.Fill( status ); 
+      } // for
+	
+   } // for
+  
+   TFile output("test.root","RECREATE");
+   ptHist.Write();    
+   statusHist.Write();
+}
+
