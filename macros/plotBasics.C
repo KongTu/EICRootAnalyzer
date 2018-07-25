7c8da0730efcb2252efec2ca020cb36b7f962684
Kong <kongPro@Kongs-MacBook-Pro-7.local>
2018-07-25 10:18:47 -0400

    add macro

--- macros/plotBasics.C
+++ macros/plotBasics.C
@@ -11,13 +11,15 @@ void plotBasics(){
 	TCanvas* c1 = new TCanvas("c1","",1,1,800,400);
 	c1->Divide(2,1);
 	c1->cd(1);
-	pz_corr->GetXaxis()->SetRangeUser(200,400);
-	pz_corr->GetYaxis()->SetRangeUser(200,400);
+	pz_corr->SetTitle("fixpf");
+	pz_corr->GetXaxis()->SetRangeUser(240,270);
+	pz_corr->GetYaxis()->SetRangeUser(240,270);
 	pz_corr->Draw("colz");
 	
 	c1->cd(2);
-	energy_corr->GetXaxis()->SetRangeUser(200,400);
-	energy_corr->GetYaxis()->SetRangeUser(200,400);
+	energy_corr->SetTitle("fixpf");
+	energy_corr->GetXaxis()->SetRangeUser(280,320);
+	energy_corr->GetYaxis()->SetRangeUser(280,320);
 	energy_corr->Draw("colz");
 
 	c1->Print("pz_corr.pdf");
