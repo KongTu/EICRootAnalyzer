1aa7913a833faf285ba40358ec4d1eb9aa170b2b
Kong <kongPro@Kongs-MacBook-Pro-7.local>
2018-07-25 14:42:46 -0400

    add macro

--- macros/plotBasics.C
+++ macros/plotBasics.C
@@ -4,24 +4,22 @@ using namespace std;
 
 void plotBasics(){
 
-	TFile* file = new TFile("test_wrongpf.root");
-	TH2D* pz_corr = (TH2D*) file->Get("pz_corr");
-	TH2D* energy_corr = (TH2D*) file->Get("energy_corr");
+	TFile* file = new TFile("wrongpf_JpsiNodecay_eD.root");
+	TH1D* pz_corr = (TH1D*) file->Get("pz_corr");
+	TH1D* energy_corr = (TH1D*) file->Get("energy_corr");
 
 	TCanvas* c1 = new TCanvas("c1","",1,1,800,400);
 	c1->Divide(2,1);
 	c1->cd(1);
-	pz_corr->SetTitle("wrongpf");
-	pz_corr->GetXaxis()->SetRangeUser(240,270);
-	pz_corr->GetYaxis()->SetRangeUser(240,270);
-	pz_corr->Draw("colz");
+	pz_corr->SetTitle("wrongpf_eD");
+	//pz_corr->GetXaxis()->SetRangeUser(240,270);
+	pz_corr->Draw("");
 	
 	c1->cd(2);
-	energy_corr->SetTitle("wrongpf");
-	energy_corr->GetXaxis()->SetRangeUser(280,320);
-	energy_corr->GetYaxis()->SetRangeUser(280,320);
-	energy_corr->Draw("colz");
+	energy_corr->SetTitle("wrongpf_eD");
+	//energy_corr->GetXaxis()->SetRangeUser(280,320);
+	energy_corr->Draw("");
 
-	c1->Print("pz_corr_wrongpf.pdf");
+	c1->Print("pz_corr_wrongpf_eD.pdf");
 
 }
