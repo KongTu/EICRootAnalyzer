71fe36aec625aef75a6fb6201607e13cd3adfc32
Kong <kongPro@Kongs-MacBook-Pro-7.local>
2018-07-25 14:47:35 -0400

    add macro

--- macros/plotBasics.C
+++ macros/plotBasics.C
@@ -4,22 +4,22 @@ using namespace std;
 
 void plotBasics(){
 
-	TFile* file = new TFile("wrongpf_JpsiNodecay_eP.root");
+	TFile* file = new TFile("fixpf_JpsiNodecay_eD.root");
 	TH1D* pz_corr = (TH1D*) file->Get("pz_corr");
 	TH1D* energy_corr = (TH1D*) file->Get("energy_corr");
 
 	TCanvas* c1 = new TCanvas("c1","",1,1,800,400);
 	c1->Divide(2,1);
 	c1->cd(1);
-	pz_corr->SetTitle("wrongpf_eP");
+	pz_corr->SetTitle("fixpf_eD");
 	//pz_corr->GetXaxis()->SetRangeUser(240,270);
 	pz_corr->Draw("");
 	
 	c1->cd(2);
-	energy_corr->SetTitle("wrongpf_eP");
+	energy_corr->SetTitle("fixpf_eD");
 	//energy_corr->GetXaxis()->SetRangeUser(280,320);
 	energy_corr->Draw("");
 
-	c1->Print("pz_corr_wrongpf_eP.pdf");
+	c1->Print("pz_corr_fixpf_eD.pdf");
 
 }
