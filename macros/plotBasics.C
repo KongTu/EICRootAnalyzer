336b9e6a9815e6d77860bc9d9451b4c3e51522e8
Kong <kongPro@Kongs-MacBook-Pro-7.local>
2018-07-25 10:17:02 -0400

    add macro

--- macros/plotBasics.C
+++ macros/plotBasics.C
@@ -9,6 +9,7 @@ void plotBasics(){
 	TH2D* energy_corr = (TH2D*) file->Get("energy_corr");
 
 	TCanvas* c1 = new TCanvas("c1","",1,1,800,400);
+	c1->Divide(2,1);
 	c1->cd(1);
 	pz_corr->GetXaxis()->SetRangeUser(200,400);
 	pz_corr->GetYaxis()->SetRangeUser(200,400);
