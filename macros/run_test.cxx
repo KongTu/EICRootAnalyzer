59ff75bcc8e0b6cb06172c862b3cef147b2c160b
Kong <kongPro@Kongs-MacBook-Pro-7.local>
2018-07-25 14:31:54 -0400

    add macro

--- macros/run_test.cxx
+++ macros/run_test.cxx
@@ -9,7 +9,7 @@
 // root [0] .L /path/to/read.cxx
 // root [1] read("myInputFile.root", 10000 )
 
-void run_test(TString inFileNames, int nEvents ) {
+void run_test(int nEvents ) {
    
    // If the analysis solely uses TTree::Draw statements, you don't need to load
    // the shared library. You will receive warnings such as
@@ -22,11 +22,11 @@ void run_test(TString inFileNames, int nEvents ) {
    
    // The TTrees are named EICTree.
    // Create a TChain for trees with this name.
-   TChain tree("EICTree" );
+   TChain tree = new TChain();
    
    // Add the file(s) we want to analyse to the chain.
    // We could add multiple files if we wanted.
-   tree.Add(inFileNames ); // Wild cards are allowed e.g. tree.Add("*.root" );
+   tree.Add("../../EICTree/eD_18x135_Q2_1_10_y_0.01_0.95_tau_7_noquench_kt=ptfrag=0.32_Shd1_ShdFac=1.32_Jpsidiffnodecay_test40k_wrongpf.root" ); // Wild cards are allowed e.g. tree.Add("*.root" );
 // tree.Add(/path/to/otherFileNames ); // etc... 
    
    // Create an object to store the current event from the tree.
@@ -116,14 +116,15 @@ void run_test(TString inFileNames, int nEvents ) {
 
       } // for
 
+      //fermi momentum
       double pxf = branch_pxf->GetValue(0,0);
       double pyf = branch_pyf->GetValue(0,0);
-
       double particle_ptF = sqrt(pxf*pxf + pyf*pyf);
+      //
 
       double particle_pt = sqrt(total4Mom_outgoing.Px()*total4Mom_outgoing.Px() + total4Mom_outgoing.Py()*total4Mom_outgoing.Py());
       
-      pTvsThat->Fill( particle_ptF, t_hat );
+      pTvsThat->Fill( particle_pt, t_hat );
       Ntrk->Fill(nParticles);
       energy_corr->Fill( total4Mom_incoming.E() - total4Mom_outgoing.E() );
       px_corr->Fill( total4Mom_incoming.Px() - total4Mom_outgoing.Px() );
