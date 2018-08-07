d4fe53a87bf5290661f5893169fb11811c4a46d8
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2018-08-07 14:31:15 -0400

    fix t_hat

--- macros/run_EvtParticlePlotter.cxx
+++ macros/run_EvtParticlePlotter.cxx
@@ -239,7 +239,7 @@ void run_EvtParticlePlotter( int nEvents, bool doBoost, TString inputFilename ) 
       
       Q2VsJpsi_91->Fill( pt_91_jpsi, trueQ2);
       Q2VsJpsi_93->Fill( pt_93_jpsi, trueQ2);
-      T_hat->Fill( pt_91_jpsi*pt_91_jpsi+trueQ2, t_hat );
+      T_hat->Fill( pt_91_jpsi*pt_91_jpsi-trueQ2, t_hat );
       
       PtVsPt_process_91_protonVsJpsi->Fill(pt_91_jpsi, pt_91_proton);
       PtVsPt_process_93_protonVsJpsi->Fill(pt_93_jpsi, pt_93_proton);//this may have more than one proton in one event
