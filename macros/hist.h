d7ea13f3124c08331457ac58ee415ff0f539380d
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2018-08-08 16:37:53 -0400

    improving

--- macros/hist.h
+++ macros/hist.h
@@ -43,7 +43,7 @@
 #define MASS_AU197    183.45406466643374
 #define MASS_PB208    193.69769264273208
 // Histograms for our analysis.
-TH1D* Ntrk_process_91 = new TH1D("Ntrk_process_91",";Ntrk_process_91", 100, 0, 100);
+TH1D* Ntrk = new TH1D("Ntrk",";Ntrk", 100, 0, 100);
 TH1D* Ntrk_process_all = new TH1D("Ntrk_process_all",";Ntrk_process_all", 100, 0, 100);
 TH1D statusHist("statusHist", "status distribution  ", 50, 0, 50 );
 
@@ -55,16 +55,16 @@ TH2D* Q2VsX = new TH2D("Q2VsX",";x;Q^{2}",10000,0.00001,1,2000,0,200);
 TH2D* W2VsFlux = new TH2D("W2VsFlux",";#Phi;W^{2}",5000,0,0.1,1000,0,10000);
 
 //TH2D:
-TH2D* PtVsEta_process_91_proton = new TH2D("PtVsEta_process_91_proton",";#eta;p_{T} (GeV)", 2000, -20,20, 200, 0,10);
-TH2D* PtVsEta_process_91_neutron = new TH2D("PtVsEta_process_91_neutron",";#eta;p_{T} (GeV)", 2000, -20,20, 200, 0,10);
+TH2D* PtVsEta_proton = new TH2D("PtVsEta_proton",";#eta;p_{T} (GeV)", 2000, -20,20, 200, 0,10);
+TH2D* PtVsEta_neutron = new TH2D("PtVsEta_neutron",";#eta;p_{T} (GeV)", 2000, -20,20, 200, 0,10);
 
-TH2D* PtVsPt_process_91_protonVsJpsi = new TH2D("PtVsPt_process_91_protonVsJpsi",";p_{T} (GeV);p_{T} (GeV)", 200, 0,10, 200, 0,10);
+TH2D* PtVsPt_protonVsJpsi = new TH2D("PtVsPt_protonVsJpsi",";p_{T} (GeV);p_{T} (GeV)", 200, 0,10, 200, 0,10);
 
-TH2D* AngleVsMom_process_91_proton = new TH2D("AngleVsMom_process_91_proton",";p (GeV);#theta (mrad)",2500,0,250,300,0,30);
-TH2D* AngleVsMom_process_91_neutron = new TH2D("AngleVsMom_process_91_neutron",";p (GeV);#theta (mrad)",2500,0,250,300,0,30);
+TH2D* AngleVsMom_proton = new TH2D("AngleVsMom_proton",";p (GeV);#theta (mrad)",2500,0,250,300,0,30);
+TH2D* AngleVsMom_neutron = new TH2D("AngleVsMom_neutron",";p (GeV);#theta (mrad)",2500,0,250,300,0,30);
 
-TH2D* AngleVssNN_process_91_proton = new TH2D("AngleVssNN_process_91_proton",";s_{_{NN}} (GeV^{2});#theta (mrad)",2500,0,10,300,0,30);
-TH2D* AngleVssNN_process_91_neutron = new TH2D("AngleVssNN_process_91_neutron",";s_{_{NN}} (GeV^{2});#theta (mrad)",2500,0,10,300,0,30);
+TH2D* AngleVssNN_proton = new TH2D("AngleVssNN_proton",";s_{_{NN}} (GeV^{2});#theta (mrad)",2500,0,10,300,0,30);
+TH2D* AngleVssNN_neutron = new TH2D("AngleVssNN_neutron",";s_{_{NN}} (GeV^{2});#theta (mrad)",2500,0,10,300,0,30);
 
 TH2D* Q2VsJpsi_91 = new TH2D("Q2VsJpsi_91",";p_{T};Q^{2}",100,0,10,2000,0,200);
 
@@ -93,18 +93,18 @@ TH1D* W2 = new TH1D("W2",";W^{2} (GeV^{2})", 1000,0,10000);
 TH1D* photonFlux = new TH1D("photonFlux",";#Phi", 5000,0,0.1);
 
 //TH1D:
-TH1D* PtDist_process_91 = new TH1D("PtDist_process_91",";PtDist_process_91", 200, 0,10);
-TH1D* PhiDist_process_91 = new TH1D("PhiDist_process_91",";PhiDist_process_91", 200, 0,10);
-TH1D* EtaDist_process_91 = new TH1D("EtaDist_process_91",";EtaDist_process_91", 2000, -20,20);
+TH1D* PtDist = new TH1D("PtDist",";PtDist", 200, 0,10);
+TH1D* PhiDist = new TH1D("PhiDist",";PhiDist", 200, 0,10);
+TH1D* EtaDist = new TH1D("EtaDist",";EtaDist", 2000, -20,20);
 
-TH1D* PtDist_process_91_Jpsi = new TH1D("PtDist_process_91_Jpsi",";PtDist_process_91_Jpsi", 200, 0,10);
-TH1D* PhiDist_process_91_Jpsi = new TH1D("PhiDist_process_91_Jpsi",";PhiDist_process_91_Jpsi", 200, 0,10);
-TH1D* EtaDist_process_91_Jpsi = new TH1D("EtaDist_process_91_Jpsi",";EtaDist_process_91_Jpsi", 2000, -20,20);
+TH1D* PtDist_Jpsi = new TH1D("PtDist_Jpsi",";PtDist_Jpsi", 200, 0,10);
+TH1D* PhiDist_Jpsi = new TH1D("PhiDist_Jpsi",";PhiDist_Jpsi", 200, 0,10);
+TH1D* EtaDist_Jpsi = new TH1D("EtaDist_Jpsi",";EtaDist_Jpsi", 2000, -20,20);
 
-TH1D* PtDist_process_91_proton = new TH1D("PtDist_process_91_proton",";PtDist_process_91_proton", 200, 0,10);
-TH1D* PhiDist_process_91_proton = new TH1D("PhiDist_process_91_proton",";PhiDist_process_91_proton", 200, 0,10);
-TH1D* EtaDist_process_91_proton = new TH1D("EtaDist_process_91_proton",";EtaDist_process_91_proton", 2000, -20,20);
+TH1D* PtDist_proton = new TH1D("PtDist_proton",";PtDist_proton", 200, 0,10);
+TH1D* PhiDist_proton = new TH1D("PhiDist_proton",";PhiDist_proton", 200, 0,10);
+TH1D* EtaDist_proton = new TH1D("EtaDist_proton",";EtaDist_proton", 2000, -20,20);
 
-TH1D* PtDist_process_91_neutron = new TH1D("PtDist_process_91_neutron",";PtDist_process_91_neutron", 200, 0,10);
-TH1D* PhiDist_process_91_neutron = new TH1D("PhiDist_process_91_neutron",";PhiDist_process_91_neutron", 200, 0,10);
-TH1D* EtaDist_process_91_neutron = new TH1D("EtaDist_process_91_neutron",";EtaDist_process_91_neutron", 2000, -20,20);
+TH1D* PtDist_neutron = new TH1D("PtDist_neutron",";PtDist_neutron", 200, 0,10);
+TH1D* PhiDist_neutron = new TH1D("PhiDist_neutron",";PhiDist_neutron", 200, 0,10);
+TH1D* EtaDist_neutron = new TH1D("EtaDist_neutron",";EtaDist_neutron", 2000, -20,20);
