a87f8f64334e949c4c311a5f1781f9f81ab1087e
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2018-08-14 11:47:38 -0400

    add kick final state

--- macros/run_KickFinalStates.cxx
+++ macros/run_KickFinalStates.cxx
@@ -1,7 +1,7 @@
 #include "hist.h"//define all the histograms
 
 TH1D* t1_dist = new TH1D("t1_dist",";t1", 200,-5,5);
-TH1D* t2_dist = new TH1D("t2_dist",";t2", 200,-5,5);
+TH1D* t2_dist = new TH1D("t2_dist",";t2", 200,-100,100);
 
 void run_KickFinalStates( int nEvents, bool doKick, TString inputFilename ) {
    
