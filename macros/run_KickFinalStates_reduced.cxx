bd830c4a10dbc00e5eab5b6bc3a30e3b43e80bc1
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2018-08-20 16:47:19 -0400

    try energy nonconservation

--- macros/run_KickFinalStates_reduced.cxx
+++ macros/run_KickFinalStates_reduced.cxx
@@ -102,8 +102,8 @@ void run_KickFinalStates_reduced( int nEvents, bool doKick, TString inputFilenam
 
          if( index == 4 ){ //get gamma 4-momentum:
 
-            particle_4mom_photon.SetPtEtaPhiM(pt,eta,phi,mass); 
-            cout << "mass of photon " << mass << endl;
+            particle_4mom_photon = particle->Get4Vector(); 
+            cout << "mass of photon " << particle_4mom_photon.M() << endl;
          }
 
          if( status != 1 ) continue; //only stable final-state particles 
