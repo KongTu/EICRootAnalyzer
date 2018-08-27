7e4c7711aa83df5623b16a987d75dc5aa4ce954d
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2018-08-27 09:52:04 -0400

    fix slow

--- macros/run_KickFinalStates_reduced.cxx
+++ macros/run_KickFinalStates_reduced.cxx
@@ -274,7 +274,7 @@ void run_KickFinalStates_reduced( int nEvents, bool doKick, TString inputFilenam
    // cout << "iter: " << i_min << " jter: " << j_min << " kter: " << k_min << endl;
    // cout << "E diff: " << E_min <<  " comp: " << comp_min << " delta: " << delta_min << " kappa: " << kappa_min << endl;
 
-   if( i_min == 0 || j_min == 0 || k_min == 0 || i_min == 9 || j_min == 99 || k_min == 9 ) continue;//hit the boundary continue;
+   // if( i_min == 0 || j_min == 0 || k_min == 0 || i_min == 9 || j_min == 99 || k_min == 9 ) continue;//hit the boundary continue;
 
    }//end of kick
 
