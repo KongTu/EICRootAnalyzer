420593d36ca16a56c1a609661d29caf75e8eb81f
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2018-08-27 10:09:35 -0400

    fix slow

--- macros/run_KickFinalStates_reduced.cxx
+++ macros/run_KickFinalStates_reduced.cxx
@@ -154,6 +154,7 @@ void run_KickFinalStates_reduced( int nEvents, bool doKick, TString inputFilenam
    
    if( doKick ){
 
+      cout << "what  " << endl;
       if( !doKick ) return;
 
       t = particle_4mom_neutron_bKick + particle_4mom_proton_bKick + particle_4mom_jpsi_bKick;
