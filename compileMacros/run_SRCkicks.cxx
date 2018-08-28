3f5d149dce7587a09866bd3ad6d795e0fb71a9c4
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2018-08-28 14:22:47 -0400

    add new macros

--- compileMacros/run_SRCkicks.cxx
+++ compileMacros/run_SRCkicks.cxx
@@ -38,13 +38,50 @@ void run_SRCkicks(int nEvents, bool doKick, TString inputFilename){
       double t_hat = event->GetHardT();
       double u_hat = event->GetHardU();
       double photon_flux = event->GetPhotonFlux();
-      Int_t event_process = event->GetProcess();
+      int event_process = event->GetProcess();
       
-      cout << "trueQ2: " << event->GetTrueQ2() << endl;
-      cout << "event_process: " << event_process << endl;
-      cout << "event ntracks: " << event->GetNTracks() << endl;
+      int nParticles = event->GetNTracks();
+      //event t_hat
 
+      // We now know the number of particles in the event, so loop over
+      // the particles:
+      int nParticles_process = 0;
 
+      TLorentzVector particle_4mom;
+      TLorentzVector t,k;
+      TLorentzVector p3,p4,p5;
+
+      TLorentzVector particle_4mom_proton_bKick;
+      TLorentzVector particle_4mom_neutron_bKick;
+      TLorentzVector particle_4mom_jpsi_bKick;
+
+      TLorentzVector particle_4mom_proton;
+      TLorentzVector particle_4mom_neutron;
+      TLorentzVector particle_4mom_jpsi;
+
+      TLorentzVector particle_4mom_photon;
+      TLorentzVector particle_4mom_electron_prime;
+
+      //if( event_process != 91 ) continue;
+      
+      /*E-M Conservation*/
+      double pztarg_1 = 135.290727;
+      double pztarg_2 = 135.103537;
+      double pz_total = pztarg_1+pztarg_2;
+      double total_energy = sqrt(pz_total*pz_total + MASS_DEUTERON*MASS_DEUTERON);
+      //electron, neglect electron mass
+      double pz_lepton = branch_pzlep->GetValue(0,0);
+      double electron_mass = 0.00051;
+      double total_lep_energy = sqrt(pz_lepton*pz_lepton + electron_mass*electron_mass);
+
+      TLorentzVector total4Mom_deuteron(0., 0., pz_total, total_energy);
+      TLorentzVector total4Mom_electron(0., 0., pz_lepton, total_lep_energy);
+
+      TLorentzVector total4Mom_outgoing(0.,0.,0.,0.);
+      TLorentzVector total4Mom_incoming = total4Mom_deuteron + total4Mom_electron;
+      /*end*/
+
+      cout << "total energy: " << total4Mom_incoming.E() << endl;
 
    } // for
 
