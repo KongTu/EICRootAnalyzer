1627420aea76c66fcaa0c2c516049b5309f34942
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2018-08-28 14:14:51 -0400

    add new macros

---
+++ compileMacros/run_SRCkicks.cxx
@@ -0,0 +1,28 @@
+#include "hist.h"//define all the histograms
+
+using namespace std;
+using namespace erhic;
+
+void run_SRCkicks(int nEvents, bool doKick, TString inputFilename){
+
+	TChain *tree = new TChain("EICTree");
+	tree->Add("../../EICTree/eD_Jpsidiffnodecay_EICTree/eD_18x135_Q2_1_10_y_0.01_0.95_tau_7_noquench_kt=ptfrag=0.32_Shd1_ShdFac=1.32_Jpsidiffnodecay_test40k_"+inputFilename+".root" ); // Wild cards are allowed e.g. tree.Add("*.root" );
+
+	EventPythia* event(NULL);// = new EventPythia;
+
+	// EventBase* event(NULL);
+	// EventBeagle* event_beagle(NULL);
+
+	tree->SetBranchAddress("event", &event ); // Note &event, not event.
+
+	//Using the TBranchElement is a hack to access the BeAGLE information.       
+	TBranchElement* branch_atarg = (TBranchElement*) tree->GetBranch("Atarg");
+	TBranchElement* branch_pz = (TBranchElement*) tree->GetBranch("pztarg");
+	TBranchElement* branch_pzlep = (TBranchElement*) tree->GetBranch("pzlep");
+	TBranchElement* branch_pxf = (TBranchElement*) tree->GetBranch("pxf");
+	TBranchElement* branch_pyf = (TBranchElement*) tree->GetBranch("pyf");
+	TBranchElement* branch_pzf = (TBranchElement*) tree->GetBranch("pzf");
+
+
+	
+}
