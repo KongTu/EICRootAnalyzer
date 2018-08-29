a125db7fb19f1cbbe29c1a5b0663abb1109d77ff
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2018-08-29 11:00:28 -0400

    kick pz as well

--- compileMacros/run_SRCkicks.cxx
+++ compileMacros/run_SRCkicks.cxx
@@ -132,9 +132,9 @@ void run_SRCkicks(int nEvents, bool doKick, TString inputFilename){
 			double kick_py = 0.;
 			double kick_pz = 0.;
 
-			TF1 *fa_pt = new TF1("fa_pt","[0]*TMath::Abs(TMath::Exp([1]*TMath::Abs(x)))",0,0.8);
+			TF1 *fa_pt = new TF1("fa_pt","[0]*TMath::Abs(TMath::Exp([1]*TMath::Abs(x)))",0,2);
 			fa_pt->SetParameter(0,1);
-			fa_pt->SetParameter(1,-10);
+			fa_pt->SetParameter(1,-3);
 			double kick_pt = fa_pt->GetRandom();
 
 			TF1 *num = new TF1("num","[0]*1",-1,1);
@@ -154,9 +154,9 @@ void run_SRCkicks(int nEvents, bool doKick, TString inputFilename){
 	
 			kick_py = kick_px*TMath::Tan(phi_kick);
 
-			TF1 *fa_pz = new TF1("fa_pz","[0]*TMath::Abs(TMath::Exp([1]*TMath::Abs(x)))",-0.8,0.8);
+			TF1 *fa_pz = new TF1("fa_pz","[0]*TMath::Abs(TMath::Exp([1]*TMath::Abs(x)))",-2,2);
 			fa_pz->SetParameter(0,1);
-			fa_pz->SetParameter(1,-0.1);
+			fa_pz->SetParameter(1,-1);
 			
 			kick_pz = fa_pz->GetRandom();
 
