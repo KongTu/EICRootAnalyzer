4f8e04677e8545979942baccafb90d55fd023d3d
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2018-08-29 12:06:58 -0400

    start messing up again

--- compileMacros/run_SRCkicks.cxx
+++ compileMacros/run_SRCkicks.cxx
@@ -352,7 +352,7 @@ void run_SRCkicks(int nEvents, bool doKick, int CASE, TString inputFilename){
 				    double py_DIFF = t.Py() - k.Py();
 				    double pz_DIFF = t.Pz() - k.Pz();
 
-				    if(E_DIFF < 0.1) cout << "E_DIFF = " << E_DIFF << endl;
+				    if(E_DIFF < 1) cout << "E_DIFF = " << E_DIFF << endl;
 					
 				}
 			}	
