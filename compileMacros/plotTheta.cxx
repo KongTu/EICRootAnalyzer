304e8835d5c7352cef6068da03d98a31b77c7481
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2018-08-30 16:23:02 -0400

    finish rotation code

--- compileMacros/plotTheta.cxx
+++ compileMacros/plotTheta.cxx
@@ -68,7 +68,7 @@ void plotTheta(int nEvents, TString inputFilename){
 		double pzf = branch_pzf->GetValue(0,0);
 		double pF = pxf*pxf + pyf*pyf + pzf*pzf;
 		
-		/*hard-coded cut*/
+		/*hard-coded cuts*/
 		if( pF < 0.3025 || pF > 0.36 ) continue;
 		if( event_process != 91 ) continue;
 		if( fabs(t_hat) > 0.1 ) continue;
