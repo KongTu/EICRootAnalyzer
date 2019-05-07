12a8f1dec4f655b99eb2d0dcd540fd7916725894
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2019-05-07 12:08:43 -0400

    test dNdy

--- compileMacros/makeComparisonE665.C
+++ compileMacros/makeComparisonE665.C
@@ -6,6 +6,8 @@ using namespace erhic;
 
 #define MASS_MUON  0.1056
 
+TH1D* dNdetaStar = new TH1D("dNdetaStar","dNdetaStar",100,-5,5);
+
 TLorentzRotation BoostToHCM(TLorentzVector const &eBeam_lab,
                             TLorentzVector const &pBeam_lab,
                             TLorentzVector const &eScat_lab) {
@@ -30,32 +32,39 @@ void makeComparisonE665(const int nEvents = 40000){
 	EventBeagle* event(NULL);
 	tree->SetBranchAddress("event", &event);
 	// EventPythia* event(NULL);
-	// tree->SetBranchAddress("event", &event );
-
-	//Using the TBranchElement is a hack to access the BeAGLE information.       
-	
+	// tree->SetBranchAddress("event", &event );	
 
 	for(int i(0); i < nEvents; ++i ) {
       
 		// Read the next entry from the tree.
 		tree->GetEntry(i);
+		
 		double pzlep = event->pzlep;
 		double pztarg = event->pztarg;
-		cout << "pzlep " << pzlep << endl;
+
+		TLorentzVector mu_beam(0.,0.,pzlep,sqrt(pzlep*pzlep+MASS_MUON*MASS_MUON));
+		TLorentzVector p_beam(0.,0.,pztarg,sqrt(pztarg*pztarg+MASS_PROTON*MASS_PROTON));
+		TLorentzVector mu_scattered(0.,0.,0.,0.);
+
 		//event information:
-		// double trueQ2 = event->GetTrueQ2();
-		// double trueW2 = event->GetTrueW2();
-		// double trueX = event->GetTrueX();
-		// double trueY = event->GetTrueY();
-		// double trueNu = event->nu;
-		// double s_hat = event->GetHardS();
-		// double t_hat = event->t_hat;
-		// double u_hat = event->GetHardU();
-		// double photon_flux = event->GetPhotonFlux();
+		double trueQ2 = event->GetTrueQ2();
+		double trueW2 = event->GetTrueW2();
+		double trueX = event->GetTrueX();
+		double trueY = event->GetTrueY();
+		double trueNu = event->nu;
+		double s_hat = event->GetHardS();
+		double t_hat = event->t_hat;
+		double u_hat = event->GetHardU();
+		double photon_flux = event->GetPhotonFlux();
 		int event_process = event->GetProcess();
 		int nParticles = event->GetNTracks();
 		int struck_nucleon = event->nucleon;
+		
 		if( event_process != 91 ) continue;
+		if( trueQ2 < 1. ) continue;
+		if( trueW2 > 30*30 || trueW2 < 8*8 ) continue;
+		if( trueX < 0.002 ) continue;
+		if( trueY > 0.85 || trueY < 0.1 ) continue;
 
 		int nParticles_process = 0;
 		for(int j(0); j < nParticles; ++j ) {
@@ -68,19 +77,32 @@ void makeComparisonE665(const int nEvents = 40000){
 			double pt = particle->GetPt();
 			double eta = particle->GetEta();
 			double phi = particle->GetPhi();
+			double rap = particle->GetRapidity();
 			double mass = particle->GetM();
 			double theta = particle->GetTheta(); 
 			theta = theta*1000.0; //change to mrad;
 			double mom = particle->GetP();
 
+			if( index == 3 ) {
+				cout << "This particle is " << pdg << endl;
+				mu_scattered.SetPtEtaPhiM(pt,eta,phi,mass);
+			}
 
+			if( mom < 0.2 || mom > 10. ) continue;
+			TLorentzRotation boost_MC_HCM = BoostToHCM(mu_beam,p_beam,mu_scattered);	
+			TLorentzVector h = particle->Get4Vector();
+			TLorentzVector hStar = boost_MC_HCM*h;
+
+			dNdetaStar->Fill( hStar.Eta() );
 
 			nParticles_process++;
 
 		} // end of particle loop
 
-		cout << "nParticles " << nParticles_process << endl;
 	}
+
+	TFile output("../rootfiles/E665_Beagle.root","RECREATE");
+	dNdetaStar->Write();
 
 
 
