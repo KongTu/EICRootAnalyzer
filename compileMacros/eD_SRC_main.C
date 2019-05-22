b0584adc4dc4dff3e2ca6b61d759c2e0f2f202fb
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2019-05-22 14:07:33 -0400

    add test

--- compileMacros/eD_SRC_main.C
+++ compileMacros/eD_SRC_main.C
@@ -13,6 +13,7 @@ TH1D* sPN = new TH1D("sPN","sPN",200,0,14);
 TH1D* sPN_4pt2 = new TH1D("sPN_4pt2","sPN_4pt2",200,0,14);
 TH1D* sPN_Jpsi = new TH1D("sPN_Jpsi","sPN_Jpsi",200,0,14);
 TH1D* sPN_Jpsi_fix = new TH1D("sPN_Jpsi_fix","sPN_Jpsi_fix",200,0,14);
+TH1D* sPN_Jpsi_fix_oneTagged = new TH1D("sPN_Jpsi_fix_oneTagged","sPN_Jpsi_fix_oneTagged",200,0,14);
 TH1D* nucleon_diff = new TH1D("nucleon_diff","nucleon_diff",2000,-10,10);
 TH1D* h_trk = new TH1D("h_trk","h_trk",50,0,50);
 
@@ -153,8 +154,7 @@ void eD_SRC_main(const int nEvents = 40000, TString filename=""){
 		n_4vect_cal = q_irf - j_4vect_irf - p_4vect_irf;
 		nucleon_diff->Fill( n_4vect_cal.E() - n_4vect_irf.E() );
 
-
-
+		sPN_Jpsi_fix_oneTagged->Fill( (p_4vect_irf+n_4vect_cal+j_4vect_irf-q_irf).Mag2() );
 
 	}
 
@@ -165,6 +165,7 @@ void eD_SRC_main(const int nEvents = 40000, TString filename=""){
 	sPN_4pt2->Write();
 	sPN_Jpsi->Write();
 	sPN_Jpsi_fix->Write();
+	sPN_Jpsi_fix_oneTagged->Write();
 	nucleon_diff->Write();
 	h_trk->Write();
 
