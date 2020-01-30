e639ad531a0c3ea21ebc77ab632282aee5f74748
Zhoudunming Tu <kong@130-199-16-253.dhcp.bnl.gov>
2020-01-29 21:45:42 -0500

    print

--- compileMacros/eD_SRC_main.C
+++ compileMacros/eD_SRC_main.C
@@ -490,7 +490,7 @@ void eD_SRC_main(const int nEvents = 40000, TString filename="", const bool doSm
 
 		TLorentzVector testp = q_irf+d_beam_irf-j_4vect_irf-struck_4vect_irf-spectator_4vect_irf;
 
-		PRINT4VECTOR(testp, 1);
+		// PRINT4VECTOR(testp, 1);
 
 		//approach 1
 		double qzkz = q_irf.Pz() - (spectator_4vect_irf.Pz());//qz-kz
@@ -512,6 +512,8 @@ void eD_SRC_main(const int nEvents = 40000, TString filename="", const bool doSm
 		double jy_new = j_4vect_irf.Py();
 		double jz_new = jz;
 		jnew.SetPxPyPzE(jx_new,jy_new,jz_new, sqrt( MASS_JPSI*MASS_JPSI + jx_new*jx_new + jy_new*jy_new + jz_new*jz_new));
+		
+		PRINT4VECTOR(q_irf+d_beam_irf-jnew-pnew-spectator_4vect_irf, 1);
 		
 		//approach 2
 		double Ennz = spectator_4vect_irf.E() + spectator_4vect_irf.Pz();
