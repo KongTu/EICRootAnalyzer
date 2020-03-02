35f3cc3d1b57ffddc580afabd16bc463495e2f6c
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2020-03-02 16:19:41 -0500

    try t for jpsi or spectator nucleon

--- compileMacros/eD_SRC_main.C
+++ compileMacros/eD_SRC_main.C
@@ -445,11 +445,10 @@ void eD_SRC_main(const int nEvents = 40000, TString filename="", const int hitNu
 		t_eej->Fill( t1_uppervtx );
 		// 2) (p - (n''))**2
 		// use LF kinematics to calculate the struck nucleon pz, E before interactions.
-		Double_t E_bInt = (alpha_stru*MASS_DEUTERON)/4. + (spectator_4vect_irf.Px()*spectator_4vect_irf.Px()+
-			spectator_4vect_irf.Py()*spectator_4vect_irf.Py()+struck_mass*struck_mass)/(alpha_stru*MASS_DEUTERON);
-		Double_t Pz_bInt = -(alpha_stru*MASS_DEUTERON)/4. + (spectator_4vect_irf.Px()*spectator_4vect_irf.Px()+
-			spectator_4vect_irf.Py()*spectator_4vect_irf.Py()+struck_mass*struck_mass)/(alpha_stru*MASS_DEUTERON);
-		
+		Double_t E_bInt = (alpha_spec*MASS_DEUTERON)/4. + (spectator_4vect_irf.Px()*spectator_4vect_irf.Px()+
+			spectator_4vect_irf.Py()*spectator_4vect_irf.Py()+struck_mass*struck_mass)/(alpha_spec*MASS_DEUTERON);
+		Double_t Pz_bInt = -(alpha_spec*MASS_DEUTERON)/4. + (spectator_4vect_irf.Px()*spectator_4vect_irf.Px()+
+			spectator_4vect_irf.Py()*spectator_4vect_irf.Py()+struck_mass*struck_mass)/(alpha_spec*MASS_DEUTERON);
 		//new 4 vector for struck nucleon before interaction;
 		TLorentzVector n_primeprime;
 		n_primeprime.SetPxPyPzE(-spectator_4vect_irf.Px(),-spectator_4vect_irf.Py(),
