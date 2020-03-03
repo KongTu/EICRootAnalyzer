691871138aeedb5743ba2690ec67c1109063d842
Zhoudunming Tu <kong@130-199-16-253.dhcp.bnl.gov>
2020-03-03 09:26:53 -0500

    try approach 2

--- compileMacros/eD_SRC_main.C
+++ compileMacros/eD_SRC_main.C
@@ -400,6 +400,8 @@ void eD_SRC_main(const int nEvents = 40000, TString filename="", const int hitNu
 		double jz_new = jz;
 		jnew.SetPxPyPzE(jx_new,jy_new,jz_new, sqrt( MASS_JPSI*MASS_JPSI + jx_new*jx_new + jy_new*jy_new + jz_new*jz_new));
 
+
+		cout << "momentum conservation ~ " << (jnew+pnew+spectator_4vect_irf - q_irf- d_beam_irf).E() << endl;
 		/*
 		- Start trying off-shell intermediate conditions
 		- Assume same proton and neutron mass. 
