58f6d6e5fa134748d5210a9daeda8635d1746c71
Zhoudunming Tu <kong@130-199-16-253.dhcp.bnl.gov>
2020-03-03 10:04:44 -0500

    try approach 2

--- compileMacros/eD_SRC_main.C
+++ compileMacros/eD_SRC_main.C
@@ -198,6 +198,7 @@ void eD_SRC_main(const int nEvents = 40000, TString filename="", const int hitNu
 	TH1D* Pz_spectator = new TH1D("Pz_spectator",";p_{z} (GeV)",200,-1,1);
 	TH1D* Pp_spectator = new TH1D("Pp_spectator",";p (GeV)",200,0,1.4);
 	TH1D* Pt_VM = new TH1D("Pt_VM",";p_{T} (GeV)",200,0,10);
+	TH1D* Pt2_VM = new TH1D("Pt2_VM",";p^{2}_{T} (GeV)",200,0,5);
 	TH1D* Pz_VM = new TH1D("Pz_VM",";p_{z} (GeV)",200,-20,20);
 	TH1D* Pp_VM = new TH1D("Pp_VM",";p (GeV)",200,0,10);
 	TH1D* alpha_spectator = new TH1D("alpha_spectator",";#alpha_{spec}",100,0,2);
@@ -548,6 +549,7 @@ void eD_SRC_main(const int nEvents = 40000, TString filename="", const int hitNu
 		//Jpsi VM 3 momentum in lab frame:
 		jnew.Boost(b);
 		Pt_VM->Fill( jnew.Pt() );
+		Pt2_VM->Fill( jnew.Pt()*jnew.Pt() );
 		Pz_VM->Fill( jnew.Pz() );
 		Pp_VM->Fill( jnew.P() );
 	}
