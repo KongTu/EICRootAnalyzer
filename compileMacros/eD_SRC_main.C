d1c809650ff6a6eba087b516658d1702491b7a3f
Zhoudunming Tu <kong@130-199-16-253.dhcp.bnl.gov>
2020-03-05 18:11:49 -0500

    check momentum conservation

--- compileMacros/eD_SRC_main.C
+++ compileMacros/eD_SRC_main.C
@@ -381,6 +381,10 @@ void eD_SRC_main(const int nEvents = 40000, TString filename="", const int hitNu
 			struck_4vect = n_4vect;
 		}
 		
+
+		TLorentzVector testp = q_irf+d_beam_irf - struck_4vect_irf - j_4vect_irf - spectator_4vect_irf;
+		cout << "momentum conservation ~ " << testp.E() << endl; 
+
 		/*
 		fixing deuteron momentum nonconservation:
 		*/
