09134bc18cba68ebb729a0a87f8f8f20d9c2fd60
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2020-11-05 12:16:21 -0500

    initial commit

--- compileMacros/eD_Tagged_DIS.C
+++ compileMacros/eD_Tagged_DIS.C
@@ -127,9 +127,8 @@ void eD_Tagged_DIS(const int nEvents = 40000, TString filename="eD_dis_Tagged_hi
 		//HERA inclusive cross section
 		double event_weight = 1.;
 		double Yc = 1. + TMath::Power((1-trueY),2);
-		event_weight = (TMath::Power(trueQ2,2)) / (twopi*alpha2*Yc);
-		// event_weight = (event_weight*mbToGeV_m2) / Lint;
-		// event_weight = event_weight / (bin_width*Q2bin);
+		event_weight = (TMath::Power(trueQ2,2)*trueX) / (twopi*alpha2*Yc);
+	
 
 		//try HERA inclusive cross section:
 		h_HERA_Q2_10_13_trueX->Fill( trueX );
