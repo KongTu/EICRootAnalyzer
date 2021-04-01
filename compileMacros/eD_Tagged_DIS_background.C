7c0d8d62a53bb7ec02cdecc68a90c2a3f1938f37
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2021-04-01 16:40:15 -0400

    write a simple algo

--- compileMacros/eD_Tagged_DIS_background.C
+++ compileMacros/eD_Tagged_DIS_background.C
@@ -171,10 +171,8 @@ void eD_Tagged_DIS_background(const int nEvents = 40000, TString filename="Outpu
 				// e_scattered = ppart;
 			}
 			if( status != 1 ) continue;
-			if( NoBAM==3 ){
-				cout << "pt " << pt << endl;
-				cout << "reference pt " << sqrt(pxf*pxf+pyf*pyf) << endl;
-			}
+			if( fbs(pt-sqrt(pxf*pxf+pyf*pyf))<1e-5 ){cout << "spectator with status and NOBAM " <<
+			status << " " << NoBAM << endl;}
 			TVector3 part; part.SetPtEtaPhi(pt, eta, phi);
 			int spec_cand = findSpectator(part, charge);
 			if( spec_cand ){
