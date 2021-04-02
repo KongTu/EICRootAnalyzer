861bc77c5463f035fc34d4f262877ddafa03554f
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2021-04-02 11:18:40 -0400

    write a simple algo

--- compileMacros/eD_Tagged_DIS_background.C
+++ compileMacros/eD_Tagged_DIS_background.C
@@ -211,7 +211,7 @@ void eD_Tagged_DIS_background(const int nEvents = 40000, TString filename="Outpu
 		//virtual photon
 		TLorentzVector qbeam = e_beam - e_scattered;
 		TLorentzVector balSys = hfsCand;
-		h_ptBalance->Fill( balSys.Pt(), e_scattered.Pt() );
+		h_ptBalance->Fill( balSys.Pt(), (e_beam+d_beam-e_scattered).Pt() );
 		//initialize spectator 4vect
 		TLorentzVector spectator_4vect_irf;
 		if(bestCandidate<0) continue;
