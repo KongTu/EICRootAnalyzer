e60176a736074b03fc1de36c7db71609cb4cc70c
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2021-04-02 13:24:17 -0400

    write a simple algo

--- compileMacros/eD_Tagged_DIS_background.C
+++ compileMacros/eD_Tagged_DIS_background.C
@@ -205,7 +205,8 @@ void eD_Tagged_DIS_background(const int nEvents = 40000, double HFSaccept=6.0, b
 			if( status!=1 ) continue;
 			TLorentzVector part4pion; part4pion.SetPtEtaPhiM(pt,eta,phi,0.13975);//assume pions
 		    //sum over HFS excluding elec' within main detector acceptance;
-		    if(!(isMatch(ppart,e_scattered)) && TMath::Abs(part4pion.Eta())<HFSaccept ) hfsCand += part4pion;
+		    // if(!(isMatch(ppart,e_scattered)) && TMath::Abs(part4pion.Eta())<HFSaccept ) hfsCand += part4pion;
+		    if(!(isMatch(ppart,e_scattered)) && !(isMatch(ppart,trueSpect)) ) hfsCand += part4pion;
 			TVector3 part; part.SetPtEtaPhi(pt, eta, phi);
 			int spec_cand = findSpectator(part, charge);
 			if( spec_cand ){
