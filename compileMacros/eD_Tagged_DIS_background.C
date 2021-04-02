250a19ecabb8ca2e102f1204533aff5ec1663c62
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2021-04-02 13:36:42 -0400

    write a simple algo

--- compileMacros/eD_Tagged_DIS_background.C
+++ compileMacros/eD_Tagged_DIS_background.C
@@ -234,7 +234,7 @@ void eD_Tagged_DIS_background(const int nEvents = 40000, double HFSaccept=6.0, b
 		h_ptBalance1D->Fill( (qbeam-hfsCand).Pt() - trueSpect.Pt() );
 		// cut pt Bal
 		if( cutPtBal_ ) {
-			if( (qbeam-hfsCand).Pt()-spectator_4vect_irf.Pt()>0.2 ) continue;
+			if( ((qbeam-hfsCand).Pt()-spectator_4vect_irf.Pt())>0.2 ) continue;
 		}
 		//algo step 1 eff.
 		h_allTagging->Fill( TMath::Power(spectator_4vect_irf.Pt(),2) );
