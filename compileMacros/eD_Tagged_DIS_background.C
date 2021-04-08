27070d427ebe005e9ab6f006eb1ebc4e7ce97f7f
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2021-04-08 14:04:27 -0400

    start making rotation

--- compileMacros/eD_Tagged_DIS_background.C
+++ compileMacros/eD_Tagged_DIS_background.C
@@ -241,7 +241,7 @@ void eD_Tagged_DIS_background(const int nEvents = 40000, double HFSaccept=4.0, b
 			}
 		}
 		TLorentzRotation rotateVector=TLorentzRotation(1.0*d_beam.BoostVector());
-		double angleTheta = (e_beam - e_scattered).Angle(d_beam);
+		double angleTheta = (e_beam - e_scattered).Angle(d_beam.Vect());
 		rotateVector.RotateY( -PI+angleTheta );
 		TLorentzVector trueSpect_lab = rotateVector*trueSpect;
 		trueSpect.Boost(b);
