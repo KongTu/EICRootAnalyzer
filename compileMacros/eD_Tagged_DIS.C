05513265ab829252bf34616b793438bfc154ad59
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2021-05-10 12:16:47 -0400

    checking alpha bound

--- compileMacros/eD_Tagged_DIS.C
+++ compileMacros/eD_Tagged_DIS.C
@@ -239,9 +239,9 @@ void eD_Tagged_DIS(const int nEvents = 40000, TString filename="Output_input_tem
 	double twopi = 2*PI;
 	double mbToGeV_m2 = 2.56819;
 	double Q2binwidth = 3.0-2.0;
-	TF1* cthetaFlat= new TF1("cthetaFlat","0.5",-1.,1.);
-	TF1* phiFlat= new TF1("phiFlat","1",-PI,PI);
-	TF1 *deutNk = new TF1("Deuteron n(k) in fm^{-1}",getdNdkstrikmanWeiss,0,1,0);
+	// TF1* cthetaFlat= new TF1("cthetaFlat","0.5",-1.,1.);
+	// TF1* phiFlat= new TF1("phiFlat","1",-PI,PI);
+	// TF1 *deutNk = new TF1("Deuteron n(k) in fm^{-1}",getdNdkstrikmanWeiss,0,1,0);
 	//alex's xbj binning
 	double xBinsArray[] = {0.0001, 0.0002, 0.0004, 0.0007, 0.001, 0.002, 0.004, 0.007, 0.01, 0.02, 0.04, 0.07, 0.1};
 	double xBinsWidth[12];
@@ -371,9 +371,10 @@ void eD_Tagged_DIS(const int nEvents = 40000, TString filename="Output_input_tem
 		//fill inclusive.
 		h_HERA_Q2_2_3->Fill( trueX, event_weight );
 
-		TF1* reweightPt2 = new TF1("reweightPt2","-1*x[0]+1.05",0.,0.05);
+		// TF1* reweightPt2 = new TF1("reweightPt2","-1*x[0]+1.05",0.,0.05);
 		double pt2 = pxf*pxf+pyf*pyf;
-		double pt2weight = reweightPt2->Eval(pt2);
+		// double pt2weight = reweightPt2->Eval(pt2);
+		double pt2weight = 1.0;
 		double alpha_spec_binwidth = -1; // will have to be rewritten by alpha defined bins
 		// double xbinwidth = 0.0007 - 0.0004;
 		double pt2binwidth = h_HERA_Q2_2_3_x_alpha[0][0]->GetBinWidth(1);
