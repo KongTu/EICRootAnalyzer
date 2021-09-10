7423d2aa6cd1e47d4dc6e7e023af2c7c95150f68
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2021-09-09 20:27:58 -0400

    forJiazhao

--- compileMacros/testBoostRotation.C
+++ compileMacros/testBoostRotation.C
@@ -71,13 +71,15 @@ TLorentzRotation BoostToHCM(TLorentzVector const &eBeam_lab,
    TLorentzVector pBoost=boost*pBeam_lab;
    TVector3 axis=pBoost.BoostVector();
 
+   // rotate away x-coordinate
+   boost.RotateY(M_PI-axis.Theta());
+
    TLorentzVector pBoost_escat=boost*eScat_lab;
    TVector3 axis_escat=pBoost_escat.BoostVector();
 
    // rotate away y-coordinate
    boost.RotateZ(-axis_escat.Phi());
-   // rotate away x-coordinate
-   boost.RotateY(M_PI-axis.Theta());
+
    return boost;
 }
 
