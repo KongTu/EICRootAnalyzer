088a404ac870c12e26c55b6fed37b38c195c52e3
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2022-01-13 17:02:01 -0500

    initial commit

--- compileMacros/eD_TaggedEMC_DIS.C
+++ compileMacros/eD_TaggedEMC_DIS.C
@@ -74,7 +74,7 @@ void eD_TaggedEMC_DIS( const int nEvents = 1e6 ){
 	tree->SetBranchAddress("event", &event);
 
 	//all constants
-	double factorInLumi = nEvents / 1e6 ;
+	double factorInLumi = nEvents / 1e5 ;
 	double totalXSection   = 1.8501226230870374E-005;; //mb
 	double nEventsTotal    = 49988*factorInLumi;
 	double Lint = nEventsTotal/totalXSection; // mb^{-1}
