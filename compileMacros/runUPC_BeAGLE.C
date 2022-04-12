375b26c38c9287fa79c34d889f73a27a06523a53
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2022-04-12 16:02:45 -0400

    add kinematics reconstruction

--- compileMacros/runUPC_BeAGLE.C
+++ compileMacros/runUPC_BeAGLE.C
@@ -206,6 +206,7 @@ void runUPC_BeAGLE(const TString filename="eA_TEST", const int nEvents = 40000, 
 		} // end of particle loop
 		
 		//Hadron only method.
+		hfs=hfs-e_scattered;//true hfs.
 		double sigma_had=hfs.E()-hfs.Pz();
 		cout << "sigma_had="<<sigma_had<<endl;
 		double measY=sigma_had/(2*e_beam.E());
